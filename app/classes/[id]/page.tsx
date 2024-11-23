e90a337ef613cd386f1ebb0ef786f5eb7a3bb702
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-11-24 05:46:59 +0700

    feat:okewes

--- app/classes/[id]/page.tsx
+++ app/classes/[id]/page.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import Classes from "@/components/Classes";
 
-interface PageProps {
-  params: { classId: string };
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function page({ params }: PageProps) {
+export default async function page() {
   return <Classes />;
 }
